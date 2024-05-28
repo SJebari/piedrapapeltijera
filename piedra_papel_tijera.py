b8cafae1513ca8fce12f3d634f45479a275b314c
Soulayman <soujebben@alu.edu.gva.es>
2024-05-28 18:41:03 +0200

    t2

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -22,14 +22,21 @@ def ganadorMovimiento(usuarioMovimiento, ordenadorMovimiento):
 
 
 print("JUEGO : Piedra, papel y tijera")
+
+nombreUsuario = str(input('Nombre del usuario: '))
+
+if len(nombreUsuario) < 3:
+    raise Exception('El nombre del usuario no puede tener menos de 3 caracteres')
+
+numeroIntentos = int(input('Numero de intentos: '))
+if numeroIntentos < 1:
+    raise Exception('El numero de intentos no puede ser menor que 1')
+
 while True:
     opcionJugar = input("Quieres jugar? (s/n): ")
     if 's' in opcionJugar.lower():
-        nombreUsuario = str(input('Nombre del usuario: '))
-
-        if len(nombreUsuario) < 3:
-            raise Exception('El nombre del usuario no puede tener menos de 3 caracteres')
-
+        usuarioGana = 0
+        ordenadorGana = 0
         opcionMovimientoOrdenador = opcionOrdenador()
         opcionMovimientoUsuario = input(
             "Selecciona un movimiento ('p' para piedra / 'a' para papel / 't' para tijeras): ").lower()
@@ -44,10 +51,24 @@ while True:
             print(f"Elección del usuario {nombreUsuario}: {opcionMovimientoUsuario}")
             if ganadorMovimiento(opcionMovimientoUsuario, opcionMovimientoOrdenador) == 1:
                 print(f"Gana el usuario {nombreUsuario}!!!")
+                usuarioGana += 1
             elif ganadorMovimiento(opcionMovimientoUsuario, opcionMovimientoOrdenador) == -1:
                 print("Gana el ordenador !!!")
+                ordenadorGana += 1
             elif ganadorMovimiento(opcionMovimientoUsuario, opcionMovimientoOrdenador) == 0:
                 print("Empate !!!")
+            numeroIntentos -= 1
+            if numeroIntentos == 0:
+                if usuarioGana > ordenadorGana and ordenadorGana < usuarioGana:
+                    print(f'\nEl usuario {nombreUsuario} gana el juego !!!\nCon {usuarioGana} partidas ganadas !!!')
+                    break
+                elif ordenadorGana > usuarioGana and usuarioGana < ordenadorGana:
+                    print(f'\nEl ordenador gana el juego !!!\nCon {ordenadorGana} partidas ganadas !!!')
+                    break
+                elif usuarioGana == ordenadorGana:
+                    print(f'\nHa habido un empate !!!\nCon {usuarioGana} partidas ganadas del {nombreUsuario}'
+                          f' y {ordenadorGana} partidas ganadas del ordenador')
+                    break
         else:
             print("Entrada incorrecta. Vuelve a intentar.")
     elif 'n' in opcionJugar.lower():
