d0ba918962830bc34c5b556270423d5b8f3d0f7d
Soulayman <soujebben@alu.edu.gva.es>
2024-05-28 18:51:21 +0200

    v3

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -27,9 +27,9 @@ while True:
     if 's' in opcionJugar.lower():
         opcionMovimientoOrdenador = opcionOrdenador()
         opcionMovimientoUsuario = input(
-            "Selecciona un movimiento ('p' para piedra / 'a' para papel / 't' para tijeras): ").lower()
-        print(f"Elección del ordenador: {opcionMovimientoOrdenador}")
-        if 'p' in opcionMovimientoUsuario or 'a' in opcionMovimientoUsuario or 't' in opcionMovimientoUsuario:
+            "Selecciona un movimiento ('p' para piedra / 'a' para papel / 't' para tijeras / 'TERMINAR' para "
+            "terminar): ").lower()
+        if opcionMovimientoUsuario == 'p' or opcionMovimientoUsuario == 'a' or opcionMovimientoUsuario == 't':
             if 'p' in opcionMovimientoUsuario and 'p' in opcionMovimientoUsuario:
                 opcionMovimientoUsuario = piedra
             elif 'a' in opcionMovimientoUsuario and 'a' in opcionMovimientoUsuario:
@@ -37,12 +37,16 @@ while True:
             elif 't' in opcionMovimientoUsuario and 't' in opcionMovimientoUsuario:
                 opcionMovimientoUsuario = tijera
             print(f"Elección del usuario: {opcionMovimientoUsuario}")
+            print(f"Elección del ordenador: {opcionMovimientoOrdenador}")
             if ganadorMovimiento(opcionMovimientoUsuario, opcionMovimientoOrdenador) == 1:
                 print("Gana el usuario !!!")
             elif ganadorMovimiento(opcionMovimientoUsuario, opcionMovimientoOrdenador) == -1:
                 print("Gana el ordenador !!!")
             elif ganadorMovimiento(opcionMovimientoUsuario, opcionMovimientoOrdenador) == 0:
                 print("Empate !!!")
+        elif opcionMovimientoUsuario == 'terminar':
+            print('Tienes miedo?')
+            break
         else:
             print("Entrada incorrecta. Vuelve a intentar.")
     elif 'n' in opcionJugar.lower():
